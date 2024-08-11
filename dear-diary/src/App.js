2649858ec482c0090d2d7c617614257cda89baab
Uttam M <uttamercer@gmail.com>
2024-08-11 21:53:55 +0000

    added sample bg and input text field

---
+++ dear-diary/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Diary from './components/diary';
+
+function App() {
+  return (
+    <>
+    <div class="diary">
+      <Diary></Diary>
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ dear-diary/src/components/diary.js
@@ -0,0 +1,9 @@
+function Diary(){
+    return (
+        <>
+        <input type = "text" class = "overlay-input" placeholder = "Write your entry here...."></input>
+        </>
+    )
+}
+
+export default Diary;
